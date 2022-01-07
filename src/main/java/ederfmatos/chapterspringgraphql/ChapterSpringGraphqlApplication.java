706d0d9945c5fec9aefec542e6221edcddda3fac
Eder Matos <ederfmatos@gmail.com>
2022-01-07 07:47:41 -0300

    Create basic project

---
+++ src/main/java/ederfmatos/chapterspringgraphql/ChapterSpringGraphqlApplication.java
@@ -0,0 +1,13 @@
+package ederfmatos.chapterspringgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterSpringGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChapterSpringGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ederfmatos/chapterspringgraphql/ChapterSpringGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ederfmatos.chapterspringgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterSpringGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
